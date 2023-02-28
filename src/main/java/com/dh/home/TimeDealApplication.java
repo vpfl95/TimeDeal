575ed748686ee1528f036c70abfaec0915daad31
vpfl95 <k_dh610@naver.com>
2023-03-01 02:53:28 +0900

    1

---
+++ src/main/java/com/dh/home/TimeDealApplication.java
@@ -0,0 +1,13 @@
+package com.dh.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeDealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeDealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dh/home/TimeDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeDealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
