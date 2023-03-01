74a48a1c2052a654f07b735972a4e79e454f8f4e
vpfl95 <k_dh610@naver.com>
2023-03-01 22:58:07 +0900

    d

---
+++ src/main/java/com/dh/home/HomeController.java
@@ -0,0 +1,15 @@
+package com.dh.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class HomeController {
+	@GetMapping("/")
+	public String home()throws Exception{
+		return "index";
+	}
+}
