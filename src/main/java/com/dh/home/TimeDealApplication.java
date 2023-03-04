b1b6f963e3cb33180b1440ecbc857cbd1914ebed
vpfl95 <k_dh610@naver.com>
2023-03-05 01:32:22 +0900

    d

--- src/main/java/com/dh/home/TimeDealApplication.java
+++ src/main/java/com/dh/home/TimeDealApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-//(exclude = DataSourceAutoConfiguration.class)
-@SpringBootApplication
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class TimeDealApplication extends SpringBootServletInitializer {
 	
 	@Override
