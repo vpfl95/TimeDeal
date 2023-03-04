e12cdde446b0e410143003e967584d0dd8b68ebe
vpfl95 <k_dh610@naver.com>
2023-03-04 22:16:18 +0900

    d

--- src/main/java/com/dh/home/TimeDealApplication.java
+++ src/main/java/com/dh/home/TimeDealApplication.java
@@ -2,8 +2,9 @@ package com.dh.home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class TimeDealApplication {
 
 	public static void main(String[] args) {
