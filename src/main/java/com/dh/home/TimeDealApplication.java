6e2a79413ff1c699eca89d8534a61ad116c1d7dc
vpfl95 <k_dh610@naver.com>
2023-03-06 21:58:47 +0900

    member

--- src/main/java/com/dh/home/TimeDealApplication.java
+++ src/main/java/com/dh/home/TimeDealApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication//(exclude = DataSourceAutoConfiguration.class)
 public class TimeDealApplication extends SpringBootServletInitializer {
 	
 	@Override
---
+++ src/main/java/com/dh/home/member/MemberVO.java
@@ -0,0 +1,10 @@
+package com.dh.home.member;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	private String id;
+	private String pw;
+	private String name;
+}
